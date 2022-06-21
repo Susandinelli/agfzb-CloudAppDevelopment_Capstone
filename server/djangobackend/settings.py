26707dffc470cb987ff6b165a2a014fb534fb3f4
Susan <susandinelli@gmail.com>
2022-06-21 15:41:39 +0000

    Commiting signup

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ['capstone-project.us-south.cf.appdomain.cloud']
+ALLOWED_HOSTS = ['localhost', 'capstone-project.us-south.cf.appdomain.cloud']
 
 
 # Application definition
